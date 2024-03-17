53ab6a5fdd0913de9d39b4fc2c8bd3346d101520
Erick Noiztbander <aventum2@gmail.com>
2024-03-17 20:36:57 +0100

    fix: utils images import

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-import productBeerUrl from './assets/images/gastro/gasto-item-beer.jpg'
-import productChickenBurgerUrl from './assets/images/gastro/gasto-item-chicken-buger.jpg'
-import productChickenWingsUrl from './assets/images/gastro/gasto-item-chicken-wings.jpg'
-import productFrenchFriesUrl from './assets/images/gastro/gasto-item-french-fries.jpg'
-import productFriedChickenUrl from './assets/images/gastro/gasto-item-fried-chicken.jpg'
-import productMilkShakeUrl from './assets/images/gastro/gasto-item-milkshake.jpg'
+import productBeerUrl from '@/assets/images/gastro/gasto-item-beer.jpg'
+import productChickenBurgerUrl from '@/assets/images/gastro/gasto-item-chicken-buger.jpg'
+import productChickenWingsUrl from '@/assets/images/gastro/gasto-item-chicken-wings.jpg'
+import productFrenchFriesUrl from '@/assets/images/gastro/gasto-item-french-fries.jpg'
+import productFriedChickenUrl from '@/assets/images/gastro/gasto-item-fried-chicken.jpg'
+import productMilkShakeUrl from '@/assets/images/gastro/gasto-item-milkshake.jpg'
 
 export function createHtmlPrimaryButton(text, action) {
   const buttonElement = document.createElement('button')
